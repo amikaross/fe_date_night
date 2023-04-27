994d6caa5914cea43eff6a83a6e0e39a7ee51329
Amanda Ross <amikaross@gmail.com>
2023-04-27 12:08:26 -0600

    Implement user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :sessions, only: [:create]
+
+  get '/', to: 'welcome#index'
+  get '/dashboard', to: 'users#show', as: :user_dashboard
 end
