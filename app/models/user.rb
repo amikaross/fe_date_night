6db0f38c6b1f8e7765a5de1f80e1e5f8eff1bfde
Amanda Ross <amikaross@gmail.com>
2023-05-04 14:54:36 -0600

    Add user update lat/long based on location

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true, email: true 
   validates_presence_of :password
+  validate :fetch_lat_and_long, on: :update
 
   has_secure_password
+
+  def fetch_lat_and_long
+    converted_location = GeocodeService.convert_address_to_latlong(location)
+    if converted_location
+      self.lat, self.long = converted_location[:lat], converted_location[:lng]
+    else
+      errors.add(:location, "cannot be an invalid address")
+    end
+  end
 end
--- spec/features/users/dashboard_spec.rb
+++ spec/features/users/dashboard_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'User Dashboard' do 
   describe 'When I visit my dashboard logged in as a valid user' do 
-    it 'I see a request to fill in my location if I have not, which when filled out is replaced by a search places bar' do
+    it 'I see a request to fill in my location if I have not, which when filled out is replaced by a list of nearby places' do
       user = User.create(email: "amanda@example.com", password: "password")
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
@@ -11,12 +11,39 @@ describe 'User Dashboard' do
 
       expect(page).to have_content("Please enter your location")
       expect(page).to have_selector('#edit_user_form')
-      within('#edit_user_form') do 
-        fill_in :location, with: '3220 N Williams St. Denver CO 80205'
-        click_button 'Submit'
+      expect(page).to_not have_selector('#nearby_places')
+      VCR.use_cassette('get_lat_long') do
+        within('#edit_user_form') do 
+          fill_in :location, with: '3220 N Williams St. Denver CO 80205'
+          click_button 'Submit'
+        end
       end
 
       expect(page).to_not have_content("Please enter your location")
+      expect(page).to_not have_selector('#edit_user_form')
+
+      expect(page).to have_selector('#nearby_places')
+      expect(user.location).to eq('3220 N Williams St. Denver CO 80205')
+      expect(user.lat).to eq('39.7624957')
+      expect(user.long).to eq('-104.9657181')
+    end
+
+    it 'if I input an address that does not return a valid lat and long, I see an error and the form to add address stays' do 
+      user = User.create(email: "amanda@example.com", password: "password")
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit user_dashboard_path
+
+      VCR.use_cassette('get_lat_long_error') do
+        within('#edit_user_form') do 
+          fill_in :location, with: 'amanda mika ross'
+          click_button 'Submit'
+        end
+      end
+
+      expect(page).to have_content("The address you entered is not valid, please try again.")
+
     end
   end
 end
