aa6fb384d15a02a9c3eaceda9cbc30489306f5b0
Amanda Ross <amikaross@gmail.com>
2023-05-05 15:16:11 -0600

    Setup places index

--- app/services/google_service.rb
+++ app/services/google_service.rb
@@ -1,4 +1,18 @@
 class GoogleService
+  def self.list_places_near(current_user)
+    response = get_nearby_places(current_user)
+  end
+
+  def self.convert_address_to_latlong(location)
+    response = geocode(location)
+    response[:results].empty? ? false : {
+                                          formatted_address: response[:results][0][:formatted_address],
+                                          lat_and_long: response[:results][0][:geometry][:location]
+                                        }
+  end
+
+  private
+
   def self.conn 
     Faraday.new(url: "https://maps.googleapis.com") do |req|
       req.params[:key] = ENV['google_key']
@@ -13,11 +27,12 @@ class GoogleService
     JSON.parse(response.body, symbolize_names: true)
   end
 
-  def self.convert_address_to_latlong(location)
-    response = geocode(location)
-    response[:results].empty? ? false : {
-                                          formatted_address: response[:results][0][:formatted_address],
-                                          lat_and_long: response[:results][0][:geometry][:location]
-                                        }
+  def self.get_nearby_places(current_user)
+    response = conn.get("/maps/api/place/nearbysearch/json") do |req|
+      req.params[:location] = "#{current_user.lat}, #{current_user.long}"
+      req.params[:radius] = "#{current_user.radius}"
+    end
+
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
---
+++ spec/features/places/index_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "Places Index" do 
+  describe "As a logged in user when I visit /places" do 
+    it "displays a list of places nearby my set location" do
+      user = User.create(email: "amanda@example.com", password: "password", location: "3220 N Williams St, Denver, CO 80205, USA", lat: "39.7624957", long: "-104.9657181" )
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      
+      VCR.use_cassette('get_places_near_cole') do 
+        visit "/places"
+      end
+    
+      expect(page).to have_content("This is a place!")
+    
+    end
+  end
+end
