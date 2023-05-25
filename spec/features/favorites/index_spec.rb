b8e0d2ee6052c1bdc71e77e52f4a9c2c9109d0bb
Amanda Ross <amikaross@gmail.com>
2023-05-25 13:08:11 -0600

    Redirect to dashboard if user tries to visit explore places without filling out location

--- spec/features/favorites/index_spec.rb
+++ spec/features/favorites/index_spec.rb
@@ -3,13 +3,19 @@ require 'rails_helper'
 describe 'Favorites Index' do 
   describe 'When I visit /favorites' do 
     describe 'As a logged in user' do 
-      it 'displays my list of favorites' do 
-        user = User.create(email: "amanda@example.com", password: "password")
+      before(:each) do 
+        @user = User.create(email: "amanda@example.com", password: "password")
         5.times do 
-          create(:favorite, user: user)
+          create(:favorite, user: @user)
         end
-require 'pry'; binding.pry
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+      end
+
+      it 'displays my list of favorites' do 
+        require 'pry'; binding.pry
+
+
       end
     end
   end
