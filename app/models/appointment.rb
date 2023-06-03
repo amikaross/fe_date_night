340c5fd653699f944c0be02d900e91ca7dcaee9d
Amanda Ross <amikaross@gmail.com>
2023-06-03 17:22:19 -0600

    Implement appointment show page

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,12 @@
 class Appointment < ApplicationRecord
   has_many :user_appointments
   has_many :users, through: :user_appointments
+
+  def formatted_time
+    time.strftime("%I:%M %p")
+  end
+
+  def formatted_date
+    date.strftime("%A, %B %-d, %Y")
+  end
 end
---
+++ spec/features/appointments/show_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+describe 'Appointment Show Page' do 
+  describe 'As a logged in user' do 
+    before(:each) do 
+      @user = User.create(email: "amanda@example.com", password: "password", location: "3220 N Williams St, Denver, CO 80205, USA", lat: "39.7624957", long: "-104.9657181" )
+      other_user = create(:user)
+      # dates the user created
+      @user_created_app1 = create(:appointment)
+      @user_created_app2 = create(:appointment)
+      # appointments the user is attending but did not create
+      @user_invited_app1 = create(:appointment)
+      @user_invited_app2 = create(:appointment)
+
+      UserAppointment.create(user: @user, appointment: @user_created_app1, owner: true)
+      UserAppointment.create(user: @user, appointment: @user_created_app2, owner: true)
+      UserAppointment.create(user: @user, appointment: @user_invited_app1, owner: false)
+      UserAppointment.create(user: @user, appointment: @user_invited_app2, owner: false)
+      UserAppointment.create(user: other_user, appointment: @user_invited_app1, owner: true)
+      UserAppointment.create(user: other_user, appointment: @user_invited_app2, owner: true)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+    end
+
+    describe 'When I visit /appointments/:id' do 
+      it 'displays the details for that particular appointment' do 
+        visit appointment_path(@user_created_app1)
+
+        expect(page).to have_content("#{@user_created_app1.name}'s Show Page")
+        expect(page).to have_content(@user_created_app1.place_name)
+        expect(page).to have_content(@user_created_app1.formatted_time)
+        expect(page).to have_content(@user_created_app1.formatted_date)
+        expect(page).to have_content(@user_created_app1.notes)
+
+
+        visit appointment_path(@user_created_app2)
+
+        expect(page).to have_content("#{@user_created_app2.name}'s Show Page")
+      end
+    end
+  end
+end
