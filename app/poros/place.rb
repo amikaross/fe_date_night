1360343b4d76ed8aaac4d83817f882a492571b6e
Amanda Ross <amikaross@gmail.com>
2023-06-08 16:51:02 -0600

    Update places index to use table

--- app/poros/place.rb
+++ app/poros/place.rb
@@ -1,10 +1,11 @@
 class Place
-  attr_reader :name, :place_id
+  attr_reader :name, :place_id, :rating, :vicinity, :types
   
   def initialize(place_data)
     @name = place_data[:name]
     @place_id = place_data[:place_id]
     @rating = place_data[:rating]
     @vicinity = place_data[:vicinity]
+    @types = place_data[:types]
   end
 end
