627111a01f97fd6b8c32118c341af57f739ac6fb
Amanda Ross <amikaross@gmail.com>
2023-06-14 15:34:57 -0600

    Remove untested error handling from favorites controller - tbr w/ application error handling

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,23 +5,16 @@ class FavoritesController < ApplicationController
 
   def create
     favorite = Favorite.new(user: current_user, google_id: params[:place_id], name: params[:name])
-    if favorite.save
-      flash[:success] = "Successfully added #{favorite.name} to favorites!"
-      redirect_to favorites_path
-    else 
-      flash[:error] = "Something went wrong!"
-      redirect_to place_path(params[:place_id])
-    end
+    favorite.save
+    flash[:success] = "Successfully added #{favorite.name} to favorites!"
+    redirect_to favorites_path
   end
 
   def destroy
     favorite = Favorite.find(params[:id])
-    if favorite
-      favorite.delete
-      flash[:success] = "Successfully deleted #{favorite.name} from favorites!"
-    else
-      flash[:error] = "Something went wrong!"
-    end
+    favorite.delete
+    flash[:success] = "Successfully deleted #{favorite.name} from favorites!"
+
     redirect_to favorites_path
   end
 end
